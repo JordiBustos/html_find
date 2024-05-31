091984b2742271f26cbfbb2e9e29b875c890186d
Jordi Bustos <jordi.bustos193771@alumnos.info.unlp.edu.ar>
2024-05-31 14:09:09 -0300

    shitty code

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use reqwest::StatusCode;
 use select::document::Document;
 use select::predicate::Name;
 use serde::Deserialize;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use url::{Position, Url};
 
 #[derive(Parser, Debug)]
@@ -27,16 +27,6 @@ struct Args {
     check_images: bool,
 }
 
-#[derive(Debug, Deserialize)]
-struct UrlSet {
-    // #[serde(rename = "url", default)]
-    // urls: Vec<UrlEntry>,
-}
-#[derive(Debug, Deserialize)]
-struct UrlEntry {
-    // #[serde(rename = "loc")]
-    // loc: String,
-}
 error_chain! {
     foreign_links {
         ReqError(reqwest::Error);
@@ -61,14 +51,31 @@ async fn check_link(url: &Url) -> Result<bool> {
     }
 }
 
-async fn find_broken_links(links: Vec<String>) -> Result<()> {
+async fn find_broken_links_or_images(
+    base_url: &Url,
+    document: &Document,
+    element: &str,
+    viewed: &mut HashMap<String, bool>,
+) -> Result<()> {
+    let base_parser = Url::options().base_url(Some(&base_url));
+
+    let links: HashSet<Url> = document
+        .find(Name(element))
+        .filter_map(|n| n.attr(if element == "a" { "href" } else { "src" }))
+        .filter_map(|link| base_parser.parse(link).ok())
+        .collect();
+
     let mut tasks = vec![];
 
     for link in links {
-        let url = Url::parse(&link)?;
-
+        if viewed.contains_key(link.as_str()) {
+            continue;
+        }
+        viewed.insert(link.as_str().to_string(), true);
         tasks.push(tokio::spawn(async move {
-            if !check_link(&url).await.unwrap() {
+            if check_link(&link).await.unwrap() {
+                println!("{} is OK", link);
+            } else {
                 println!("{} is Broken", link);
             }
         }));
@@ -107,48 +114,51 @@ async fn get_document(url: &Url) -> Result<Document> {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let args = Args::parse();
-    let url = Url::parse(&args.url)?;
-    let document = get_document(&url).await?;
-    let base_url = get_base_url(&url, &document).await?;
+    let args: Args = Args::parse();
+    let url: Url = Url::parse(&args.url)?;
+    let document: Document = get_document(&url).await?;
+    let base_url: Url = get_base_url(&url, &document).await?;
+    let mut viewed: HashMap<String, bool> = HashMap::new();
 
     println!("Starting...");
     if args.is_xml_sitemap {
         let urls = extract_urls(&document);
         let filtered_urls = filter_urls(urls, &base_url);
 
-        if args.links {
-            // xml links to each xml page, page=1, page=2, etc
-            for url in filtered_urls {
-                let internal_document = get_document(&Url::parse(&url)?).await?;
-                let internal_urls = extract_urls(&internal_document);
-                let internal_filtered_urls = filter_urls(internal_urls, &base_url);
+        // xml links to each xml page, page=1, page=2, etc
+        for url in filtered_urls {
+            if viewed.contains_key(&url) {
+                continue;
+            }
+            viewed.insert(url.clone(), true);
 
-                find_broken_links(internal_filtered_urls).await?;
+            let internal_document = get_document(&Url::parse(&url)?).await?;
+            let internal_urls = extract_urls(&internal_document);
+            let internal_filtered_urls = filter_urls(internal_urls, &base_url);
+
+            for internal_url in internal_filtered_urls {
+                if viewed.contains_key(&internal_url) {
+                    continue;
+                }
+                viewed.insert(internal_url.clone(), true);
+
+                let internal_document = get_document(&Url::parse(&internal_url)?).await?;
+                if args.links {
+                    find_broken_links_or_images(&base_url, &internal_document, "a", &mut viewed)
+                        .await?;
+                }
+                if args.check_images {
+                    find_broken_links_or_images(&base_url, &internal_document, "img", &mut viewed)
+                        .await?;
+                }
             }
         }
     } else {
-        let base_parser = Url::options().base_url(Some(&base_url));
-
-        let links: HashSet<Url> = document
-            .find(Name(if args.check_images { "img" } else { "a" }))
-            .filter_map(|n| n.attr("href").or(n.attr("src")))
-            .filter_map(|link| base_parser.parse(link).ok())
-            .collect();
-        let mut tasks = vec![];
-
-        for link in links {
-            tasks.push(tokio::spawn(async move {
-                if check_link(&link).await.unwrap() {
-                    println!("{} is OK", link);
-                } else {
-                    println!("{} is Broken", link);
-                }
-            }));
+        if args.links {
+            find_broken_links_or_images(&base_url, &document, "a", &mut viewed).await?;
         }
-
-        for task in tasks {
-            task.await?
+        if args.check_images {
+            find_broken_links_or_images(&base_url, &document, "img", &mut viewed).await?;
         }
     }
 
